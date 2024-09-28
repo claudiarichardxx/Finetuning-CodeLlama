1b82e04f56e0b08115c716ceba47c2599068ff4d
claudiarichardxx <claudiarichardxx@gmail.com>
2024-09-28 17:14:40 -0400

    path

--- eval/humaneval.py
+++ eval/humaneval.py
@@ -20,7 +20,7 @@ class HumanEval:
 
     def read_problems(self):
 
-        self.HUMAN_EVAL = "HumanEval.jsonl.gz"
+        self.HUMAN_EVAL = "eval/HumanEval.jsonl.gz"
         evalset_file = self.HUMAN_EVAL
         return {task["task_id"]: task for task in self.stream_jsonl(evalset_file)}
 
